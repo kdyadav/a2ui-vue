5660b1b4edadf1fdc2f165c925c7a1b5ee50fce4
Kuldeep Yadav <kuldeep.yadav3012@gmail.com>
2026-01-29 14:22:08 +0700

    Remove obsolete JSONL files for assistant, dashboard, products, status, and taskmanager streams

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import GeminiSimulation from './simple-a2ui/GeminiSimulation.vue';
 import "./App.css"
-createApp(GeminiSimulation).mount('#app')
+createApp(App).mount('#app')
